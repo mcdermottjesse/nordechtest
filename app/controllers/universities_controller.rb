b13a658996cc5cea3e76000385504b604aaf4941
Jesse McDermott <mcdermott.jesse9@gmail.com>
2022-02-11 12:09:02 -0800

    finalize code and format for submission

--- app/controllers/universities_controller.rb
+++ app/controllers/universities_controller.rb
@@ -24,10 +24,10 @@ class UniversitiesController < ApplicationController
     @canada = parsed_response_canada
     @usa = parsed_response_usa
 
-    # if !University.exists?
-    #   parsed_response_canada.map do |university|
-    #     University.create(university_name: university["name"])
-    #   end
-    # end
+    if !University.exists?
+      parsed_response_canada.map do |university|
+        University.create(university_name: university["name"])
+      end
+    end
   end
 end
